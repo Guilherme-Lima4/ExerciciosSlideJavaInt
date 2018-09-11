cfb29bbca21dfb5c01534baf19a4873f62de4ce5
henriquegallodias <henrique0521@hotmail.com>
2018-09-10 22:36:29 -0300

    Exercicios java

---
+++ src/exercicios/ExerciciosSlideJavaInt.java
@@ -0,0 +1,34 @@
+package exercicios;
+
+public class ExerciciosSlideJavaInt {
+
+    
+	public static void main(String[] args) {
+	
+	/*
+	7
+	int x = 12 * 12;
+	double pi = 3.1415;
+	
+	System.out.println(pi * x);
+	/
+	 
+	 
+	8
+	int resto = 99 % 4;
+	System.out.println(resto);
+	/*
+	 9			
+	int x = 20;
+	double divi = (x * 0.5);
+	System.out.println(divi);
+	/*
+	 10
+	int num = 8;
+	double mul = num+num+num+num+num+num+num+num;
+	System.out.println(mul);
+	/* 
+	 */
+	}
+
+}
